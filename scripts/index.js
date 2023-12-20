4dfbe25158b528318dd9a8488ed7282fda5c59f6
Dalton Robertson <Robertsond744@gmail.com>
2023-12-20 14:27:44 -0600

    sprint 4 stage 2

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,55 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+const profileEditButton = document.querySelector(".profile__arrow");
+const profileEditModal = document.querySelector("#modal-edit");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subheading");
+
+const modalInputName = profileEditModal.querySelector("#modal__input-name");
+const modalInputDescription = document.querySelector(
+  "#modal__input-description"
+);
+
+const profileModalClose = document.querySelector(".modal__close");
+
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".card-wrapper");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.link;
+  return cardElement;
+}
+
+profileEditButton.addEventListener("click", () => {
+  modalInputName.value = profileName.textContent;
+  modalInputDescription.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileModalClose.addEventListener("click", () => {
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileName.textContent = modalInputName.value;
+  profileDescription.textContent = modalInputDescription.value;
+  closePopup();
+});
+
+initialCards.forEach(function (cardData) {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
